ce6038ec7098cc4c5c6b1c42ae55a974be17c34a
lolxdfly <lolxdfly@gmail.com>
2020-04-28 20:24:04 +0200

    GET Test

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 // SETUP
 // ------------------------------------------------
 
+function findGetParameter(parameterName) {
+    var result = null,
+        tmp = [];
+    location.search
+        .substr(1)
+        .split("&")
+        .forEach(function (item) {
+          tmp = item.split("=");
+          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
+        });
+    return result;
+}
+console.log(findGetParameter("example"));
+
 var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 camera.position.z = 4;
