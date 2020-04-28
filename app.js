46f62bff2beeb9055702f483c7316505f0350487
mich101mich <mich101mich@gmail.com>
2020-04-28 20:41:27 +0200

    some vital fixes

--- app.js
+++ app.js
@@ -3,26 +3,16 @@
 // ------------------------------------------------
 
 function findGetParameter(parameterName) {
-    var result = null,
-        tmp = [];
-    location.search
-        .substr(1)
-        .split("&")
-        .forEach(function (item) {
-          tmp = item.split("=");
-          if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-        });
-    return result;
+    return new URL(location).searchParams.get(parameterName);
 }
-console.log();
 
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.z = 4;
-var renderer = new THREE.WebGLRenderer({antialias:true});
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setClearColor("#000000");
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
 // ------------------------------------------------
 // FUN STARTS HERE
@@ -30,45 +20,41 @@ document.body.appendChild( renderer.domElement );
 
 // create random vertices
 var vertices = [];
-for(var i = 0; i < 10000; i++ ) {
-	var x = THREE.MathUtils.randFloatSpread( 2000 );
-	var y = THREE.MathUtils.randFloatSpread( 2000 );
-	var z = THREE.MathUtils.randFloatSpread( 2000 );
-	vertices.push( x, y, z );
+for (var i = 0; i < 10000; i++) {
+    var x = THREE.MathUtils.randFloatSpread(2000);
+    var y = THREE.MathUtils.randFloatSpread(2000);
+    var z = THREE.MathUtils.randFloatSpread(2000);
+    vertices.push(x, y, z);
 }
 
 // add points to scene
 var geometry = new THREE.BufferGeometry();
-geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-var material = new THREE.PointsMaterial( { color: 0x888888 } );
-var points = new THREE.Points( geometry, material );
-scene.add( points );
+geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
+var material = new THREE.PointsMaterial({ color: 0x888888 });
+var points = new THREE.Points(geometry, material);
+scene.add(points);
 
-// clock for time
-var clock = new THREE.Clock();
-
-function simpleParticle() {
-    // get delta time
-    var dt = clock.getDelta();
-
+function simpleParticle(dt) {
     // move all points in one direction
     // r_p = r_p + v_p * dt
-    points.position.x = points.position.x + 20 * dt;
-    points.position.y = points.position.y + 20 * dt;
-    points.position.z = points.position.z + 20 * dt;
+    points.position.x += 20 * dt;
+    points.position.y += 20 * dt;
+    points.position.z += 20 * dt;
 }
 
+let time = 0.0;
 
 // Render Loop
-var render = function() {
-    requestAnimationFrame( render );
+var render = function (now = 0.0) {
+    requestAnimationFrame(render);
+    const delta = time - now;
+    time = now;
 
     // tests:
-    switch(findGetParameter("example"))
-    {
-        case "0": simpleParticle(); break;
+    switch (findGetParameter("example")) {
+        case "0": simpleParticle(delta); break;
         case "1": break;
-    }    
+    }
 
     // Render the scene
     renderer.render(scene, camera);
